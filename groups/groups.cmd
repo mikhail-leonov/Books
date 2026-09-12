6860991c4dc25d33f3877d6c742f7b1c7d05c923
Mikhail Leonov <mikecommon@gmail.com>
2026-09-12 12:07:36 -0400

    Auto backup 2026-09-12-12-07-34

--- groups/groups.cmd
+++ groups/groups.cmd
@@ -11,4 +11,6 @@ node groups.js
 copy "groups\Harry Potter.sql"  "..\src\sql\groups\Harry Potter.sql" 
 copy "groups\EVE Online.sql"    "..\src\sql\groups\EVE Online.sql" 
 copy "groups\Popadanec.sql"     "..\src\sql\groups\Popadanec.sql" 
+copy "groups\S-T-I-K-S.sql"     "..\src\sql\groups\S-T-I-K-S.sql"
 copy "groups\Erotica.sql"       "..\src\sql\groups\Erotica.sql" 
+copy "groups\RPG.sql"           "..\src\sql\groups\RPG.sql" 
--- src/sql/groups.sql
+++ src/sql/groups.sql
@@ -1,4 +1,7 @@
-INSERT INTO groups (id, name) VALUES (1, 'Potter') ON DUPLICATE KEY UPDATE name = VALUES(name);
-INSERT INTO groups (id, name) VALUES (2, 'Содружество') ON DUPLICATE KEY UPDATE name = VALUES(name);
-INSERT INTO groups (id, name) VALUES (3, 'Stulchick') ON DUPLICATE KEY UPDATE name = VALUES(name);
+INSERT INTO groups (id, name) VALUES (1, 'Harry Potter') ON DUPLICATE KEY UPDATE name = VALUES(name);
+INSERT INTO groups (id, name) VALUES (2, 'EVE Online') ON DUPLICATE KEY UPDATE name = VALUES(name);
+INSERT INTO groups (id, name) VALUES (3, 'Popadanec') ON DUPLICATE KEY UPDATE name = VALUES(name);
+INSERT INTO groups (id, name) VALUES (4, 'Erotica') ON DUPLICATE KEY UPDATE name = VALUES(name);
+INSERT INTO groups (id, name) VALUES (5, 'S-T-I-K-S') ON DUPLICATE KEY UPDATE name = VALUES(name);
+INSERT INTO groups (id, name) VALUES (6, 'RPG') ON DUPLICATE KEY UPDATE name = VALUES(name);
  
